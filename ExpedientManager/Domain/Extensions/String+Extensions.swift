12395060578b316b0851dc6c2f20c2d7c57123ac
ivanportales <gidsp.snf19@uea.edu.br>
2024-02-09 21:19:32 -0300

    feat: new loading view for LoadingProtocol

--- ExpedientManager/Domain/Extensions/String+Extensions.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension StringProtocol {
-    var firstUppercased: String {
-        return prefix(1).uppercased() + dropFirst()
-    }
-}
--- Pods/Target Support Files/Pods-ExpedientManagerTests/Pods-ExpedientManagerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ExpedientManagerTests : NSObject
-@end
-@implementation PodsDummy_Pods_ExpedientManagerTests
-@end
