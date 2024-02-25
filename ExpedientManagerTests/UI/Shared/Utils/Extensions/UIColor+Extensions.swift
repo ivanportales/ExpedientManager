fb1b864da9619d1d5340ac241a326364bf6e2b5e
ivanportales <gidsp.snf19@uea.edu.br>
2024-02-25 17:50:12 -0300

    tests: added UIColor+Extensions

---
+++ ExpedientManagerTests/UI/Shared/Utils/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Extensions.swift
+//  ExpedientManagerTests
+//
+//  Created by Gonzalo Ivan Santos Portales on 25/02/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static var mockColors: [UIColor] {
+        return [
+            UIColor(hex: "#0000FF"),
+            UIColor(hex: "#FF2D55"),
+            UIColor(hex: "#00FF00"),
+            UIColor(hex: "#800080"),
+            UIColor(hex: "#FF8000"),
+            UIColor(hex: "#101138"),
+            UIColor(hex: "#0000FF"),
+            UIColor(hex: "#FF2D55"),
+            UIColor(hex: "#00FF00"),
+            UIColor(hex: "#800080")
+        ]
+    }
+}
