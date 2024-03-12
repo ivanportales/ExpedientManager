012cd887378043c19e44a9d903899d562fb04a63
ivanportales <gidsp.snf19@uea.edu.br>
2024-03-12 16:43:51 -0300

    tests: added testTap on  ButtonView+Extensions

--- ExpedientManagerTests/UI/Shared/Utils/Extensions/ButtonView+Extensions.swift
+++ ExpedientManagerTests/UI/Shared/Utils/Extensions/ButtonView+Extensions.swift
@@ -5,4 +5,10 @@
 //  Created by Gonzalo Ivan Santos Portales on 12/03/24.
 //
 
-import Foundation
+@testable import ExpedientManager
+
+extension ButtonView {
+    func testTap() {
+        touchDownCompletion?(self)
+    }
+}
