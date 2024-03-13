e7621695727134c91ebf51dde770cc2822d78dcc
ivanportales <gidsp.snf19@uea.edu.br>
2024-03-13 10:41:38 -0300

    fix: renamed SaveScheduledNotificationUseCase, it had the wrong name on the file

--- ExpedientManager/Domain/UseCases/SaveScheduledNotificationUseCase/SaveScheduledNotificationUseCaseProtocol.swift
+++ ExpedientManager/Domain/UseCases/SaveScheduledNotificationUseCase/SaveScheduledNotificationUseCaseProtocol.swift
@@ -7,34 +7,7 @@
 
 import Foundation
 
-final class SaveScheduledNotificationUseCase {
-    
-    // MARK: - Private Properties
-    
-    private let scheduledNotificationsRepository: ScheduledNotificationRepositoryProtocol
-    private let notificationManager: UserNotificationsManagerProtocol
-    
-    // MARK: - Init
-    
-    init(scheduledNotificationsRepository: ScheduledNotificationRepositoryProtocol,
-         notificationManager: UserNotificationsManagerProtocol) {
-        self.scheduledNotificationsRepository = scheduledNotificationsRepository
-        self.notificationManager = notificationManager
-    }
-    
-    // MARK: - Exposed Functions
-    
+protocol SaveScheduledNotificationUseCaseProtocol {
     func save(scheduledNotification: ScheduledNotification,
-              completion: @escaping (Result<Bool, Error>) -> ()) {
-        scheduledNotificationsRepository.save(scheduledNotification: scheduledNotification) { [weak self] result in
-            switch result {
-            case .failure(let error):
-                completion(.failure(error))
-            case .success(_):
-                guard let self = self else { return }
-                self.notificationManager.set(scheduledNotification: scheduledNotification,
-                                             completion: completion)
-            }
-        }
-    }
+              completion: @escaping (Result<Bool, Error>) -> ())
 }
