b24930ae43334385bedb5af98912b34356426f51
ivanportales <gidsp.snf19@uea.edu.br>
2024-03-13 10:40:01 -0300

    add: created SaveScheduledNotificationUseCase

---
+++ ExpedientManager/Domain/UseCases/SaveScheduledNotificationUseCase/SaveScheduledNotificationUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  SaveScheduledNotificationUseCase.swift
+//  ExpedientManager
+//
+//  Created by Gonzalo Ivan Santos Portales on 13/03/24.
+//
+
+import Foundation
+
+protocol SaveScheduledNotificationUseCaseProtocol {
+    func save(scheduledNotification: ScheduledNotification,
+              completion: @escaping (Result<Bool, Error>) -> ())
+}
---
+++ ExpedientManager/Domain/UseCases/SaveScheduledNotificationUseCase/SaveScheduledNotificationUseCaseProtocol.swift
@@ -0,0 +1,40 @@
+//
+//  SaveScheduledNotificationUseCaseProtocol.swift
+//  ExpedientManager
+//
+//  Created by Gonzalo Ivan Santos Portales on 13/03/24.
+//
+
+import Foundation
+
+final class SaveScheduledNotificationUseCase {
+    
+    // MARK: - Private Properties
+    
+    private let scheduledNotificationsRepository: ScheduledNotificationRepositoryProtocol
+    private let notificationManager: UserNotificationsManagerProtocol
+    
+    // MARK: - Init
+    
+    init(scheduledNotificationsRepository: ScheduledNotificationRepositoryProtocol,
+         notificationManager: UserNotificationsManagerProtocol) {
+        self.scheduledNotificationsRepository = scheduledNotificationsRepository
+        self.notificationManager = notificationManager
+    }
+    
+    // MARK: - Exposed Functions
+    
+    func save(scheduledNotification: ScheduledNotification,
+              completion: @escaping (Result<Bool, Error>) -> ()) {
+        scheduledNotificationsRepository.save(scheduledNotification: scheduledNotification) { [weak self] result in
+            switch result {
+            case .failure(let error):
+                completion(.failure(error))
+            case .success(_):
+                guard let self = self else { return }
+                self.notificationManager.set(scheduledNotification: scheduledNotification,
+                                             completion: completion)
+            }
+        }
+    }
+}
