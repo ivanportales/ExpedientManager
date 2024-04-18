b037ba1c471f79110fd6d599b1d6803d1e8bef2a
ivanportales <gidsp.snf19@uea.edu.br>
2024-04-17 23:04:45 -0300

    tests: added test_empty_state() on ScheduledNotificationListTableViewTests

--- ExpedientManagerTests/UI/Shared Views/ScheduledNotificationListTableViewTests.swift
+++ ExpedientManagerTests/UI/Shared Views/ScheduledNotificationListTableViewTests.swift
@@ -1,5 +1,5 @@
 //
-//  ScaleTests.swift
+//  ScheduledNotificationListTableViewTests.swift
 //  ExpedientManagerTests
 //
 //  Created by Gonzalo Ivan Santos Portales on 17/04/24.
@@ -11,21 +11,30 @@ import XCTest
 final class ScheduledNotificationListTableViewTests: XCTestCase {
     
     private var tableView: ScheduledNotificationListTableView!
-    
-    override func setUp() {
-        super.setUp()
-        tableView = ScheduledNotificationListTableView(scheduledNotifications: [], emptyListMessage: "No notifications")
-    }
-    
-    override func tearDown() {
-        tableView = nil
-        super.tearDown()
-    }
+    private let emptyListMessage = "No notifications"
     
     func test_setup_scheduledNotifications() {
-        let mockNotifications = [ScheduledNotification(), ScheduledNotification()]
+        makeSUT()
+        
+        let mockNotifications = ScheduledNotification.getModels()
         tableView.setup(scheduledNotifications: mockNotifications)
         
         XCTAssertEqual(tableView.numberOfRows(inSection: 0), mockNotifications.count)
+    }
+    
+    func test_empty_state() {
+        makeSUT()
+        tableView.setup(scheduledNotifications: [])
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? EmptyTableViewCell
+        
+        XCTAssertNotNil(cell)
+        XCTAssertEqual(cell?.messageLabel.text, emptyListMessage)
+    }
+    
+    func makeSUT() {
+        tableView = ScheduledNotificationListTableView(scheduledNotifications: [], 
+                                                       emptyListMessage: "No notifications")
     }
 }
