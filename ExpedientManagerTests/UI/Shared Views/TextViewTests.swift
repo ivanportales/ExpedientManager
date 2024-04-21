41c607f4ce181fc0c134da3a2311931b53dc68b6
ivanportales <gidsp.snf19@uea.edu.br>
2024-04-20 22:41:05 -0300

    tests: added test_placeholder_isSetWhenTextViewEndsEditingWithEmptyText() on TextViewTests

--- ExpedientManagerTests/UI/Shared Views/TextViewTests.swift
+++ ExpedientManagerTests/UI/Shared Views/TextViewTests.swift
@@ -26,6 +26,15 @@ final class TextViewTests: XCTestCase {
         XCTAssertEqual(textView.textColor, .black)
     }
     
+    func test_placeholder_isSetWhenTextViewEndsEditingWithEmptyText() {
+        makeSUT()
+        textView.textViewDidBeginEditing(textView)
+        textView.textViewDidEndEditing(textView)
+        
+        XCTAssertEqual(textView.text, "Placeholder")
+        XCTAssertEqual(textView.textColor, .lightGray)
+    }
+    
     func makeSUT() {
         textView = TextView(placeholder: "Placeholder", textColor: .black)
     }
