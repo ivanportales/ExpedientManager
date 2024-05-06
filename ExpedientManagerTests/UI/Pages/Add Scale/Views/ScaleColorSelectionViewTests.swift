f832e95acb1db73acd71b7a9da1ec4294890fab8
ivanportales <gidsp.snf19@uea.edu.br>
2024-05-06 17:27:41 -0300

    tests: added test_setupWithSelectedColor() on ScaleColorSelectionViewTests

--- ExpedientManagerTests/UI/Pages/Add Scale/Views/ScaleColorSelectionViewTests.swift
+++ ExpedientManagerTests/UI/Pages/Add Scale/Views/ScaleColorSelectionViewTests.swift
@@ -17,6 +17,15 @@ class ScaleColorSelectionViewTests: XCTestCase {
         XCTAssertEqual(sut.selectedColor, sut.colors.first!)
         XCTAssertEqual(sut.numberOfItems(inSection: 0), sut.colors.count)
     }
+    
+    func test_setupWithSelectedColor() {
+        makeSUT()
+        
+        let selectedColor = UIColor.selectionRed
+        sut.setup(selectedColor: selectedColor)
+
+        XCTAssertEqual(sut.selectedColor, selectedColor)
+    }
 
     private func makeSUT() {
         sut = ScaleColorSelectionView()
